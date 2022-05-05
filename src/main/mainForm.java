c700b29ed639f05d6c7ae6f6ad681674b3416aef
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-05-05 21:54:44 +0200

    Modifica la dimensione della finestra

--- src/main/mainForm.java
+++ src/main/mainForm.java
@@ -53,7 +53,7 @@ public class mainForm extends javax.swing.JFrame {
             InstantiationException, IllegalAccessException, IllegalArgumentException,
             InvocationTargetException, IOException, URISyntaxException {
         initComponents();
-        Food food = new Food.Builder(1024, 700, Food.Distribution.square, 500).build();
+        Food food = new Food.Builder(1024, 640, Food.Distribution.square, 500).build();
         Batterio.setFood(food);
         inizializzaBatteri();
         this.jPanelTerrain.add(new Terrain(
